679209209854605a2a314f8cc905af02b9e2837c
BlythePollard <github email address>
2020-05-16 12:08:56 -0600

    added api namespacing to controllers & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :messages
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :messages
+    end
+  end
 end
